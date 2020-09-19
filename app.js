4c4fddb9cbf473067259f10b68a6cc7fd044fb03
Adesh <>
2020-09-19 16:35:50 +0000

    Added Mongo connecion

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 
-//onst url = 'mongodb://localhost/EnquiriesDB'
-const url = 'mongodb://biztrack-mongodb:m1vKtk7LoEmrGxFxcaqcJkduzwska3OWtW2UtGvtWa5JBj1SsYrgoreEY3lh7DCbKnGO1F5GFr7oIe8rmZTShQ==@biztrack-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@biztrack-mongodb@'
+//const url = 'mongodb://localhost/EnquiriesDB'
+const url = 'mongodb://biztrack-mongodb:m1vKtk7LoEmrGxFxcaqcJkduzwska3OWtW2UtGvtWa5JBj1SsYrgoreEY3lh7DCbKnGO1F5GFr7oIe8rmZTShQ==@biztrack-mongodb.mongo.cosmos.azure.com:10255/?ssl=true&replicaSet=globaldb&retrywrites=false&maxIdleTimeMS=120000&appName=@biztrack-mongodb@/EnquiriesDB'
 
 const app = express()
 app.use(cors({
@@ -24,5 +24,5 @@ const quotationRouter = require('./routes/quotation')
 app.use('/quotations', quotationRouter)
 
 app.listen(8080, () => {
-    console.log("Server Started and listening on 9000")
+    console.log("Server Started and listening on 8080")
 })
